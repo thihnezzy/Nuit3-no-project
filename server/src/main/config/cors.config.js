ac8dbf3f569743ab01529c47d147688ef17e91a6
NGUYEN KE AN <nkalk192002@gmail.com>
2023-12-05 00:45:40 +0100

    server: add frontend uri to cors config

--- server/src/main/config/cors.config.js
+++ server/src/main/config/cors.config.js
@@ -1,6 +1,10 @@
 let corsOptions = {
     origin: function(origin, callback) {
-        let allowed_origin = ["http://localhost:3000", "http://localhost:5173", "https://nuit3-thihnezzy.vercel.app"]
+        let allowed_origin = [  "http://localhost:3000",
+                                        "http://localhost:5173",
+                                        "https://nuit3-thihnezzy.vercel.app",
+                                        "https://nuitinfo-frontend-dragon-ac1039363ffb.herokuapp.com/"
+        ]
         if(allowed_origin.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         }else {
